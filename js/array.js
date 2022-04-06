82778f66e86eee270a4dc91d584c38d965c91e35
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-06 13:19:25 +0300

    Create array.js

---
+++ js/array.js
@@ -0,0 +1,13 @@
+const planets = [ 
+    'Mercury', 
+    'Venus', 
+    'Earth',
+    'Mars',
+    'Jupiter'
+];
+
+console.log(planets[0]);
+console.log(planets[1]);
+console.log(planets[2]);
+console.log(planets[3]);
+console.log(planets[4]);
